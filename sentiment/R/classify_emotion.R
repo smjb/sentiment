fff9dbd1a302ef7a77a35b675a757b7533b6d981
smjb <syed.mohamad@outlook.com>
2015-11-15 09:53:33 +0800

    attempt to accelerate classify_emotion and classify_polarity by streaming out to file instead of using rbind. rbind is very slow in a large loop. still wip, not ready for packages

--- sentiment/R/classify_emotion.R
+++ sentiment/R/classify_emotion.R
@@ -1,17 +1,29 @@
-classify_emotion <- function(textColumns,algorithm="bayes",prior=1.0,verbose=FALSE,...) {
-	matrix <- create_matrix(textColumns,...)
-	lexicon <- read.csv(system.file("data/emotions.csv.gz",package="sentiment"),header=FALSE)
-
-	counts <- list(anger=length(which(lexicon[,2]=="anger")),disgust=length(which(lexicon[,2]=="disgust")),fear=length(which(lexicon[,2]=="fear")),joy=length(which(lexicon[,2]=="joy")),sadness=length(which(lexicon[,2]=="sadness")),surprise=length(which(lexicon[,2]=="surprise")),total=nrow(lexicon))
-	documents <- c()
-
-	for (i in 1:nrow(matrix)) {
-		if (verbose) print(paste("DOCUMENT",i))
-		scores <- list(anger=0,disgust=0,fear=0,joy=0,sadness=0,surprise=0)
-		doc <- matrix[i,]
-		words <- findFreqTerms(doc,lowfreq=1)
-		
-		for (word in words) {
+classify_emotion <- function(textColumns,algorithm="bayes",prior=1.0,
+                             verbose=FALSE,verbosestep=FALSE,...) {
+    fname <- tempfile("sentiment_emotion", fileext=".csv")
+    if (verbose) print(paste("TEMP FILE : ",fname))
+    zz <- file(fname, "w")
+    
+    matrix <- create_matrix(textColumns,...)
+#    lexicon <- read.csv(system.file("data/emotions.csv.gz",package="sentiment"),header=FALSE)
+    lexicon <- read.csv("emotions.csv.gz",header=FALSE)
+    
+    counts <- list(anger=length(which(lexicon[,2]=="anger")),disgust=length(which(lexicon[,2]=="disgust")),fear=length(which(lexicon[,2]=="fear")),joy=length(which(lexicon[,2]=="joy")),sadness=length(which(lexicon[,2]=="sadness")),surprise=length(which(lexicon[,2]=="surprise")),total=nrow(lexicon))
+    #documents <- c()
+    if(verbosestep==F) verbosestep=100
+    stepval <- round(seq(from=1,to=nrow(matrix), length.out=verbosestep),0)
+    www<-1;
+    nrm <- nrow(matrix)
+    for (i in 1:nrm) {
+        if (verbose & i==stepval[www]) {
+            print(paste("DOCUMENT",i, "of", nrm))
+            www<-www+1
+        }
+        scores <- list(anger=0,disgust=0,fear=0,joy=0,sadness=0,surprise=0)
+        doc <- matrix[i,]
+        words <- findFreqTerms(doc,lowfreq=1)
+        
+        for (word in words) {
             for (key in names(scores)) {
                 emotions <- lexicon[which(lexicon[,2]==key),]
                 index <- pmatch(word,emotions[,1],nomatch=0)
@@ -20,11 +32,11 @@ classify_emotion <- function(textColumns,algorithm="bayes",prior=1.0,verbose=FAL
                     
                     category <- as.character(entry[[2]])
                     count <- counts[[category]]
-        
+                    
                     score <- 1.0
                     if (algorithm=="bayes") score <- abs(log(score*prior/count))
-            
-                    if (verbose) {
+                    
+                    if (verbose==2) {
                         print(paste("WORD:",word,"CAT:",category,"SCORE:",score))
                     }
                     
@@ -45,12 +57,16 @@ classify_emotion <- function(textColumns,algorithm="bayes",prior=1.0,verbose=FAL
                 scores[[key]] <- scores[[key]]+0.000001
             }
         }
-		
+        
         best_fit <- names(scores)[which.max(unlist(scores))]
         if (best_fit == "disgust" && as.numeric(unlist(scores[2]))-3.09234 < .01) best_fit <- NA
-		documents <- rbind(documents,c(scores$anger,scores$disgust,scores$fear,scores$joy,scores$sadness,scores$surprise,best_fit))
-	}
-	
-	colnames(documents) <- c("ANGER","DISGUST","FEAR","JOY","SADNESS","SURPRISE","BEST_FIT")
-	return(documents)
+        #documents <- rbind(documents,c(scores$anger,scores$disgust,scores$fear,scores$joy,scores$sadness,scores$surprise,best_fit))
+        q <- paste(i,scores$anger,scores$disgust,scores$fear,scores$joy,scores$sadness,scores$surprise,best_fit , sep=",")
+        cat(q, file=zz,sep="\n")
+    }
+    close(zz)
+    documents <- read.csv(fname, header=F)
+    unlink(fname)
+    colnames(documents) <- c("LINENUMBER","ANGER","DISGUST","FEAR","JOY","SADNESS","SURPRISE","BEST_FIT")
+    return(documents)
 }
--- sentiment/R/classify_polarity.R
+++ sentiment/R/classify_polarity.R
@@ -1,60 +1,76 @@
-classify_polarity <- function(textColumns,algorithm="bayes",pstrong=0.5,pweak=1.0,prior=1.0,verbose=FALSE,...) {
-	matrix <- create_matrix(textColumns,...)
-	lexicon <- read.csv(system.file("data/subjectivity.csv.gz",package="sentiment"),header=FALSE)
+classify_polarity <- function(textColumns,algorithm="bayes",pstrong=0.5,pweak=1.0,prior=1.0,
+                              verbose=FALSE,verbosestep=FALSE,...) {
+    fname <- tempfile("sentiment_polarity", fileext=".csv")
+    zz <- file(fname, "w")
+    matrix <- create_matrix(textColumns,...)
+#    lexicon <- read.csv(system.file("data/subjectivity.csv.gz",package="sentiment"),header=FALSE)
+    lexicon <- read.csv("subjectivity.csv.gz",header=FALSE)
+    
+    counts <- list(positive=length(which(lexicon[,3]=="positive")),negative=length(which(lexicon[,3]=="negative")),total=nrow(lexicon))
+    documents <- c()
 
-	counts <- list(positive=length(which(lexicon[,3]=="positive")),negative=length(which(lexicon[,3]=="negative")),total=nrow(lexicon))
-	documents <- c()
-
-	for (i in 1:nrow(matrix)) {
-		if (verbose) print(paste("DOCUMENT",i))
-		scores <- list(positive=0,negative=0)
-		doc <- matrix[i,]
-		words <- findFreqTerms(doc,lowfreq=1)
-		
-		for (word in words) {
-			index <- pmatch(word,lexicon[,1],nomatch=0)
-			if (index > 0) {
-				entry <- lexicon[index,]
-				
-				polarity <- as.character(entry[[2]])
-				category <- as.character(entry[[3]])
-				count <- counts[[category]]
-	
-				score <- pweak
+    if(verbosestep==F) verbosestep=100
+    stepval <- round(seq(from=1,to=nrow(matrix), length.out=verbosestep),0)
+    www<-1;
+    
+    nrm<-nrow(matrix)
+    for (i in 1:nrm) {
+        if (verbose && i==stepval[www]) {
+            print(paste("DOCUMENT",i))
+            www<-www+1;
+        }
+        scores <- list(positive=0,negative=0)
+        doc <- matrix[i,]
+        words <- findFreqTerms(doc,lowfreq=1)
+        
+        for (word in words) {
+            index <- pmatch(word,lexicon[,1],nomatch=0)
+            if (index > 0) {
+                entry <- lexicon[index,]
+                
+                polarity <- as.character(entry[[2]])
+                category <- as.character(entry[[3]])
+                count <- counts[[category]]
+                
+                score <- pweak
                 if (polarity == "strongsubj") score <- pstrong
-				if (algorithm=="bayes") score <- abs(log(score*prior/count))
-		
-				if (verbose) {
+                if (algorithm=="bayes") score <- abs(log(score*prior/count))
+                
+                if (verbose==3) {
                     print(paste("WORD:",word,"CAT:",category,"POL:",polarity,"SCORE:",score))
-				}
-				
-				scores[[category]] <- scores[[category]]+score
-			}		
-		}
-		
-		if (algorithm=="bayes") {
-			for (key in names(scores)) {
-				count <- counts[[key]]
-				total <- counts[["total"]]
-				score <- abs(log(count/total))
-				scores[[key]] <- scores[[key]]+score
-			}
-		} else {
-			for (key in names(scores)) {
-				scores[[key]] <- scores[[key]]+0.000001
-			}
-		}
-		
+                }
+                
+                scores[[category]] <- scores[[category]]+score
+            }		
+        }
+        
+        if (algorithm=="bayes") {
+            for (key in names(scores)) {
+                count <- counts[[key]]
+                total <- counts[["total"]]
+                score <- abs(log(count/total))
+                scores[[key]] <- scores[[key]]+score
+            }
+        } else {
+            for (key in names(scores)) {
+                scores[[key]] <- scores[[key]]+0.000001
+            }
+        }
+        
         best_fit <- names(scores)[which.max(unlist(scores))]
         ratio <- as.integer(abs(scores$positive/scores$negative))
         if (ratio==1) best_fit <- "neutral"
-		documents <- rbind(documents,c(scores$positive,scores$negative,abs(scores$positive/scores$negative),best_fit))
-		if (verbose) {
-			print(paste("POS:",scores$positive,"NEG:",scores$negative,"RATIO:",abs(scores$positive/scores$negative)))
-			cat("\n")
-		}
-	}
-	
-	colnames(documents) <- c("POS","NEG","POS/NEG","BEST_FIT")
-	return(documents)
+#        documents <- rbind(documents,c(scores$positive,scores$negative,abs(scores$positive/scores$negative),best_fit))
+        q <- paste(i,scores$positive,scores$negative,abs(scores$positive/scores$negative),best_fit, sep=",")
+        cat(q, file=zz,sep="\n")
+        if (verbose>=2) {
+            print(paste("POS:",scores$positive,"NEG:",scores$negative,"RATIO:",abs(scores$positive/scores$negative)))
+            cat("\n")
+        }
+    }
+    close(zz)
+    documents <- read.csv(fname,header=F)
+    #unlink(fname)
+    colnames(documents) <- c("LINENUMBER","POS","NEG","POS/NEG","BEST_FIT")
+    return(documents)
 }
